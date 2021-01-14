d8a37f914a7bb1f21cb4179c1f659be0ba5067c4
Dan <spqjeks@naver.com>
2021-01-14 18:51:01 +0900

    nice to do this

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
+import './env';
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+import passport from 'passport';
+import { authenticateJwt } from './passport';
 
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 4040;
-
-
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ schema, context: ({ request }) => ({ request }) });
 
 server.express.use(logger('dev'));
+server.express.use(authenticateJwt);
 
 server.start({ port:PORT },()=>console.log(`(●'◡'●) Server running on port http://localhost:${PORT}`));
