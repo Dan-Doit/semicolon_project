7de7fe6f3571a6c65955b5497dfb5a6211318d0a
Dan <spqjeks@naver.com>
2021-02-06 21:59:51 +0900

    fix notofocation ahth

--- src/api/Message/getMessages/getMessages.js
+++ src/api/Message/getMessages/getMessages.js
@@ -5,9 +5,8 @@ export default {
   Query: {
     getMessages: (_, args, { request }) => {
       isAuthenticated(request);
-        //   const { RoomId } = args;
-          const RoomId = "ckkqfcpg4d99w0928ro6hozpl";
-          return prisma.messages({ where: { room: { id: RoomId } } })
+          const { roomId } = args;
+          return prisma.messages({ where: { room: { id: roomId } } })
     }
   }
 };
